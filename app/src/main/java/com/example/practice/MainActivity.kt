bef2d834dc0c4679d7ba33b7a6e14f6e571f0024
Navjot2104 <gnavjotkaur572@gmail.com>
2025-06-26 19:35:12 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/practice/MainActivity.kt
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.practice
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+var btn:Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        btn=findViewById(R.id.bnn)
+        btn?.setOnClickListener {
+            Toast.makeText(this,"next",Toast.LENGTH_SHORT).show()
+            var intent=Intent(this,spinnerprac::class.java)
+            startActivity(intent)
+        }
+
     }
 }
